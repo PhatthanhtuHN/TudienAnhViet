1048cd78932670b94f7f94685231b98bde8b4575
PhatthanhtuHN <SRV.Vietnamese@outlook.com>
2020-10-16 19:53:51 +0700

    Update

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,19 @@
 package sample;
 
+import Tudien.DictionaryManagement;
+
+import java.io.IOException;
+
 public class Controller {
+    DictionaryManagement dictionaryManagement;
+
+
+    public Controller() throws IOException {
+        dictionaryManagement = new DictionaryManagement();
+        dictionaryManagement.readFromFile();
+    }
+
+    public void submit(){
+
+    }
 }
